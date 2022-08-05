7717ce795c2b13bc5f3e50040470529dba2ef2b9
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-05 20:44:17 +0200

    js output, js identifiers, js variables

--- JAVA SCRIPT/2. Cas/app.js
+++ JAVA SCRIPT/2. Cas/app.js
@@ -1,1 +1,29 @@
-console.log(typeof 1);
+//                                                      Javascript output
+// Postoji nekoliko nacina da prikazemo podatke u javascriptu
+
+// 1.innerHTML
+// primer
+
+document.getElementById("paragraf").innerHTML = "Javascript";
+
+// 2. document.write() - Koristi se za testiranje koda.
+
+// 3. alert() - ova metoda nam salje upozorenje
+// primer:
+alert("Pozdrav ");
+
+// 4. console.log() - najcesce koriscena funkcija za prikazivanje stvari u konzoli.
+// primer
+console.log("Pozdrav iz konzole");
+
+//                                                      Java script Identifiers/Names
+// javascript names (imena varijabli) moraju pocinjati sa:
+// 1. slovom(velkim ili malim)
+// 2. dollar sign ($)
+// 3. underscore ( donja crta _)
+// Napomena.
+// Promenljiva ne moze pocinjati moze. Ali brojevi se mogu naci kao drugi,treci..... karakter u nazivu promenljive.
+
+//                                      Javascript is case sensitive (razlikuje velika i mala slova)
+"a" != "A";
+"A" != "a";
