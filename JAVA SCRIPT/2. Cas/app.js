b16a6990ff59066c5b07ac6d416a733b3101c9cc
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-05 21:03:11 +0200

    js output, js identifiers, js variables

--- JAVA SCRIPT/2. Cas/app.js
+++ JAVA SCRIPT/2. Cas/app.js
@@ -27,3 +27,18 @@ console.log("Pozdrav iz konzole");
 //                                      Javascript is case sensitive (razlikuje velika i mala slova)
 "a" != "A";
 "A" != "a";
+
+//                                                       Javascript variables
+// Postoje nekoliko nacina da deklarisemo varijablu:
+// 1. Koriscenjem var kljucne reci (keyword)
+var x;   // deklaracija
+x = 10; // dodela vrednosti promenljivoj x
+var x = 10; // odjednom izvrsena deklaracija i inicijalizacija.
+// 2. Koriscenjem let kljucne reci (keyword)
+let x;       // deklaracija;
+x = 10;     // dodela vrednosti promenljivoj x
+let x = 10;
+// 3. Koriscenjem const kljucne reci(keyword)
+const x = 10; // Kod const kljucne reci odjednom se mora izvrsiti deklaracija i inicijalizacija.
+
+// Varijable (promenljive) su kontejneri za skladistenje vrednosti.
