00bb9fe3a960c0896777a296885a6a28d8f7de92
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-12 20:57:56 +0200

    kondicionali i zadaci

--- JAVA SCRIPT/5. Cas/app.js
+++ JAVA SCRIPT/5. Cas/app.js
@@ -117,3 +117,11 @@ switch (broj) {
 	default:
 		console.log("Niste uneli broj iz trazenog opsega");
 }
+
+// DOMACI
+// Napisati program koji na osnovu dva uneta broja od strane korisnika ispisuje:
+// Povrsinu kvadrata ako su uneti brojevi jednaki,
+// Povrsinu pravogaonika ako su uneti brojevi razliciti.
+
+// Korisnik unosi dva realna broja x i y napisati program koji izracunava i stampa kolicnik x/y ako je broj y != 0,
+// a inace ispisuje poruku deljenje nemoguce.
