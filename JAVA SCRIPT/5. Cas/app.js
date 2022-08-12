38612cddd786fef05f37aa41a2fc8e3f14e4c350
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-12 19:52:13 +0200

    kondicionali

---
+++ JAVA SCRIPT/5. Cas/app.js
@@ -0,0 +1,10 @@
+//      Kondicionali
+// u JS se if narebda pise na sledeci nacin:
+
+if (uslov1) {
+	// ako je ispunjen uslov1 izvrsiti odredjeni kod
+} else if (uslov2) {
+	// ako je ispunjen uslov2 izvrsiti odredjeni kod
+} else {
+	// u slucaju da nije zadovoljen ni prvi ni drugi uslov, izvrsiti odredjeni kod
+}
