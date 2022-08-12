c4009a3f5febefee755f4e00abd3d3cd9b3a5f20
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-12 22:24:42 +0200

    domaci

--- DOMACI JS/1. Domaci/app.js
+++ DOMACI JS/1. Domaci/app.js
@@ -5,3 +5,21 @@
 
 // Korisnik unosi dva realna broja x i y napisati program koji izracunava i stampa kolicnik x/y ako je broj y != 0,
 // a inace ispisuje poruku deljenje nemoguce.
+
+// const a = Number(prompt("Unesite prvi broj"));
+// const b = Number(prompt("Unesite drugi broj"));
+
+// if (a === b) {
+// 	console.log("Povrsina kvadrata je", a * a);
+// } else if (a !== b) {
+// 	console.log("Povrsina pravougaonika je", a * b);
+// }
+
+const x = Number(prompt("Unesite x"));
+const y = Number(prompt("Unesite y"));
+
+if (y !== 0) {
+	console.log(x / y);
+} else if (y === 0) {
+	console.log("Deljenje nije moguce");
+}
