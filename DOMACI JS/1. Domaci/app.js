d7d9f5f02cb12a0ff7ff4f36f205155ec189248f
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-12 22:12:49 +0200

    domaci

---
+++ DOMACI JS/1. Domaci/app.js
@@ -0,0 +1,7 @@
+// DOMACI
+// Napisati program koji na osnovu dva uneta broja od strane korisnika ispisuje:
+// Povrsinu kvadrata ako su uneti brojevi jednaki,
+// Povrsinu pravogaonika ako su uneti brojevi razliciti.
+
+// Korisnik unosi dva realna broja x i y napisati program koji izracunava i stampa kolicnik x/y ako je broj y != 0,
+// a inace ispisuje poruku deljenje nemoguce.
