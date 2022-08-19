58a10487b14c6588700316d3eeb5755c88af335a
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-19 20:24:38 +0200

    vezbanje

--- JAVA SCRIPT/8. Cas/app.js
+++ JAVA SCRIPT/8. Cas/app.js
@@ -1,4 +1,4 @@
-// Prebrojati i sabrati brojeve deljive sa 5 u intervalu od 1 do n
+// Prebrojati i brojeve deljive sa 5 u intervalu od 1 do n
 
 let x = 1;
 let n = Number(prompt("Unesite broj"));
@@ -6,12 +6,17 @@ let n = Number(prompt("Unesite broj"));
 let suma = 0;
 
 let brojDeljivih = 0;
-
-for (x; x <= n; x++) {
-	if (x % 5 === 0) {
-		suma += n;
-		brojDeljivih += 1;
+if (isNaN(n) || n < 1) {
+	console.log("niste uneli prirodan broj");
+} else {
+	for (x; x <= n; x++) {
+		if (x % 5 === 0) {
+			suma += x;
+			brojDeljivih += 1;
+		}
 	}
+	console.log(
+		"Broj deljivih brojeva od 1 do " + n + " sa 5 je: " + brojDeljivih + "."
+	);
+	console.log("Suma deljivih brojeva sa 5 je: " + suma);
 }
-console.log(suma);
-console.log(brojDeljivih);
