0bede507ff9a93a234b86823b3b308568c4d9b13
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-19 20:59:10 +0200

    vezbanje

--- JAVA SCRIPT/8. Cas/app.js
+++ JAVA SCRIPT/8. Cas/app.js
@@ -23,8 +23,22 @@
 
 // Neka se izvrsi iteracija od broja 99 do -99. Ispisati zbir broja deljivog sa 4 i broja 14.
 
-for (let i = 99; i >= -99; i--) {
-	if (i % 4 === 0) {
-		console.log(i);
+// for (let i = 99; i >= -99; i--) {
+// 	if (i % 4 === 0) {
+// 		console.log(i);
+// 	}
+// }
+
+// Izracunati aritmetikcu sredinu brojeva koji su deljivi sa 3.
+// Iteracija se vrsi od 3 do 17
+let brojBrojeva = 0;
+let zbirBrojeva = 0;
+for (let i = 3; i < 18; i++) {
+	if (i % 3 === 0) {
+		brojBrojeva += 1;
+		zbirBrojeva += i;
 	}
 }
+
+aritSredina = zbirBrojeva / brojBrojeva;
+console.log("Aritmetika sredina broejva je: " + aritSredina);
