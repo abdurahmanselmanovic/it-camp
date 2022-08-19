3489af7b5fb1d5afb007224fa714b067887d1dec
abdurahman <selmanovicabdurrahman@gmail.com>
2022-08-19 20:15:22 +0200

    vezbanje

--- JAVA SCRIPT/8. Cas/app.js
+++ JAVA SCRIPT/8. Cas/app.js
@@ -1,13 +1,17 @@
-// Prebrojati brojeve deljive sa 5 u intervalu od 1 do n
+// Prebrojati i sabrati brojeve deljive sa 5 u intervalu od 1 do n
 
-x = 1;
-n = Number(prompt("Unesite broj"));
+let x = 1;
+let n = Number(prompt("Unesite broj"));
 
-suma = 0;
+let suma = 0;
+
+let brojDeljivih = 0;
 
 for (x; x <= n; x++) {
 	if (x % 5 === 0) {
 		suma += n;
+		brojDeljivih += 1;
 	}
 }
 console.log(suma);
+console.log(brojDeljivih);
