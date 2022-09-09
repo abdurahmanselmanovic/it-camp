d59251874eccf3d7020a3bf9b63ef71f94b5dc5d
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-09 21:10:32 +0200

    truthy i falsy vrednosti i metode sa stringovima

--- Javascript 2. ciklus/3. Cas/app2.js
+++ Javascript 2. ciklus/3. Cas/app2.js
@@ -26,3 +26,17 @@ let novaRecenica =
 	"Neka nova recenica koja treba sadrzati backslash kao deo stridjasdjaskdjaklsdjaskljdkasjda\
     jsdhasgdhjasgdgasdkasdlasdjkasjkdjjksajkdjkasjkdjkasjkdjkjk\
     asjkdjkasjkdjkasjkdjkasjkdjkasjkdjkaksjdjkajksjkdjkaskdjkasjkdjkasjkdkaskdkasjkasdnga";
+
+// 3 metode za uzimanje dela stringa
+// 1. slice(start, end) - end se ne ukljucuje
+// 2. substring (start, end)end se ne ukljucuje (Razlika u odnosu na slice je u tome sto substring ne prima negative vrednosti)
+// 3.substr(start, lenght)
+
+let novaRecenica3 =
+	"Ovo je jedna duza recenica koju treba skratiti mozemo primeniti sve tri navedene metode.";
+console.log(novaRecenica3.length);
+
+console.log(novaRecenica3.slice(7, 26));
+console.log(novaRecenica3.slice(-7, -1));
+console.log(novaRecenica3.substring(7, 12));
+console.log(novaRecenica3.substr(7, 12));
