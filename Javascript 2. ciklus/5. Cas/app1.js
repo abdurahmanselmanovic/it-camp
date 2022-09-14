c311d600200377ddb0a91dc11b534e9470a87123
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-14 21:02:29 +0200

    Hoisting, trim metoda, charAt metoda

--- Javascript 2. ciklus/5. Cas/app1.js
+++ Javascript 2. ciklus/5. Cas/app1.js
@@ -6,3 +6,15 @@ console.log(recenica.length);
 let skracenaRecenica = recenica.trim();
 console.log(skracenaRecenica);
 console.log(skracenaRecenica.length);
+
+// Postoje 3 metoda za ekstraktovanje stringova:
+// 1. charAt(position,)
+// 2. chatCodeAt(position)
+// 3. pritupanje preko[]
+console.log(skracenaRecenica.charAt(13));
+console.log(skracenaRecenica.charCodeAt(13));
+console.log(skracenaRecenica[13]);
+// Pristupanje nepostojecem indeksu se odrzava razlicito u zavisnosti od toga da li koristimo
+// metodu charAt() ili pristupamo preko []
+console.log(skracenaRecenica.charAt(126));
+console.log(skracenaRecenica[126]);
