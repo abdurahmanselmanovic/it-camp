22ef815f54ff3cb57aaaa9981b17a4d83daa7c58
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-16 19:48:43 +0200

    6. cas

---
+++ Javascript 2. ciklus/6. Cas/app.js
@@ -0,0 +1,9 @@
+// split() metoda ce se izvrsiti  konverziju stringa u niz(Array).
+
+var recenica = "Zelimo izvrsiti konverziju ove recenice u niz.";
+var recenica2 =
+	"Ako bi se desilo da na pocetku casa dodju svi na vreme, to bi bilo cudo\
+    ,koje do tog trenutka nismo videli, mada je zadovaljavajuce da svakom casu prisustvuje\
+    vecina polaznika kursa.";
+
+console.log(recenica2);
