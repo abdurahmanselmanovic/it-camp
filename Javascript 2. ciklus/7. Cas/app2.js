1b905cecc335246d8613f0ce7d039800613ae80d
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-19 20:52:19 +0200

    Back-Ticks, startsWith(), endsWith(), interpolacija

--- Javascript 2. ciklus/7. Cas/app2.js
+++ Javascript 2. ciklus/7. Cas/app2.js
@@ -1,15 +1,31 @@
-// Konverter centimetra u inch
+// // Konverter centimetra u inch
+
+// const myFunction = () => {
+// 	const centimetri = +prompt("Unesite broj centimetara");
+// 	return centimetri / 2.54;
+// };
+
+// console.log(`${myFunction()}`);
+
+// // Konvertovanje kilometara u metare
+// const myFunction2 = (kilometri) => {
+// 	return kilometri * 1000;
+// };
+
+// console.log(`${myFunction2(10)}`);
 
 const myFunction = () => {
-	const centimetri = +prompt("Unesite broj centimetara");
-	return centimetri / 2.54;
+	const duzina = +prompt("Unesite duzinu");
+	const sirina = +prompt("Unesite sirinu");
+	if (isNaN(duzina) || isNaN(sirina)) {
+		return "Uneli ste neodgovarajuce vrednosti";
+	} else if (duzina < 0 || duzina >= 100) {
+		return "Opseg duzine moze biti izmedju 0 i 100";
+	} else if (sirina < 0 || sirina >= 100) {
+		return "Opseg sirine moze biti izmedju 0 i 100";
+	} else {
+		return 2 * duzina + 2 * sirina;
+	}
 };
 
-console.log(`${myFunction()}`);
-
-// Konvertovanje kilometara u metare
-const myFunction2 = (kilometri) => {
-	return kilometri * 1000;
-};
-
-console.log(`${myFunction2(10)}`);
+console.log(myFunction());
