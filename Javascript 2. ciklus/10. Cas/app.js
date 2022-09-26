cb88c33ce4d3e5f81fbb5de71aef150ef5cf3e81
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-26 20:02:15 +0200

    zadaci sa domaceg

---
+++ Javascript 2. ciklus/10. Cas/app.js
@@ -0,0 +1,17 @@
+// const myFunction = () => {
+// 	const recenica = prompt("Unesite neku recenicu");
+// 	let novaRecenica = "";
+// 	let duzina = recenica.length;
+// 	for (let i = 0; i < recenica.length; i++) {
+// 		if (duzina % 2 === 1 && i === duzina - 1) {
+// 			novaRecenica += recenica[i];
+// 		} else if (i % 2 === 0) {
+// 			novaRecenica += recenica[i + 1];
+// 		} else if (i % 2 === 1) {
+// 			novaRecenica += recenica[i - 1];
+// 		}
+// 	}
+// 	return novaRecenica;
+// };
+
+// console.log(myFunction());
