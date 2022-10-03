f141d8c6bc371044758d119af11e51e928a83ecb
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-03 23:31:25 +0200

    Number properties

---
+++ Javascript 2. ciklus/13. Cas/app2.js
@@ -0,0 +1,48 @@
+// JS Math object //
+
+// Math Object nam dozvoljava primenjivanje matematickih zadataka na brojeve.
+
+// Math Properties (Constants)
+
+// Sintaxa je: Math.Property
+// JavaScript nam dozvoljava 8 matematickih konstanti (Properties)
+
+// 1. Math.E
+
+console.log(Math.E);
+
+// 2. Math.PI
+
+console.log(Math.PI);
+
+// 3. Math_SQRT2 - vraca kvadratni koren broja  2
+
+console.log(Math.SQRT2);
+console.log(Number(Math.SQRT2.toFixed(2)));
+
+// 4. Math_SQRT1_2 - vraca kvadratni koren broj 1/2
+
+console.log(Math.SQRT1_2);
+console.log(Number(Math.SQRT1_2.toFixed(2)));
+
+// 5. Math_LN2 - vraca nam logaritam broja 2 za osnovu e
+
+console.log(Math.LN2);
+// 6. Math_LN10 vraca nam logaritam broja 10 za osnovu e
+
+console.log(Math.LN10);
+
+// 7. Math.LOG2E vraca logaritam broja e za osnovu 2
+
+console.log(Math.LOG2E);
+
+// 8. Math.LOG10E vraca logaritam broja e za osnovu 10
+
+console.log(Math.LOG10E);
+
+// Domaci
+// 1. napraviti funkciju koja izracunava obim kruga, gde se poluprecnik unosi od strane
+// korisnika
+// 2. napraviti funkciju koja izracunava povrsinu kruga, gde poluprenik predstavlja argument
+// funkcije
+// 3. napraviti funkciju za pretvaranje radijana u stepene.
