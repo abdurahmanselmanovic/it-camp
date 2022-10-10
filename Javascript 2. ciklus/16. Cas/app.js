31ab9d68bce32b1f3932c0b3739ff1ce8d1d47ec
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-10 20:55:20 +0200

    nizovi

---
+++ Javascript 2. ciklus/16. Cas/app.js
@@ -0,0 +1,15 @@
+let x = 26;
+
+let mussa = 16;
+let aminab = 18;
+let aminam = 18;
+let harun = 18;
+let muhamed = 17;
+let abdurahman = 14;
+let talib = 15;
+let imran = 19;
+
+let godine = [16, 18, 18, 18, 17, 14, 15, 19];
+
+console.log(abdurahman);
+console.log(godine[5] - godine[5]);
