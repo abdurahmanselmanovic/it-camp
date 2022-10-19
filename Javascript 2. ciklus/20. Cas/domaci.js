be9721ae2989c226ea57334bbf8e357c4410ba66
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-19 20:04:10 +0200

    Domaci

--- Javascript 2. ciklus/20. Cas/domaci.js
+++ Javascript 2. ciklus/20. Cas/domaci.js
@@ -9,3 +9,15 @@ const firstTask = (arr) => {
 };
 
 console.log(firstTask([1, 2, 3, 4, 5, 6, 7, 8, -32, -12, -23]));
+
+const secondTask = (arr) => {
+	const neparni = [];
+	for (let broj of arr) {
+		if (broj % 2 !== 0 && !isNaN(broj)) {
+			neparni.push(broj);
+		}
+	}
+	return neparni;
+};
+
+console.log(secondTask([1, 2, 3, 4, 5, 6, 7, 8, -32, -12, -23]));
