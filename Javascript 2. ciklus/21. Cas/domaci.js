b203f6501410c7560e5d94cb3e55a9363ffa7bab
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-21 19:40:07 +0200

    domaci i push metode

---
+++ Javascript 2. ciklus/21. Cas/domaci.js
@@ -0,0 +1,10 @@
+const myfunction = (arr) => {
+	let maximum = Number.NEGATIVE_INFINITY;
+	for (number of arr) {
+		if (number > maximum) {
+			maximum = number;
+		}
+	}
+	return maximum;
+};
+console.log(myfunction([12, -32, 32, 13, 52, 32, 17]));
