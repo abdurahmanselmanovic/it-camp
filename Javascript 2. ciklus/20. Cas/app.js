71c0e4aa4dcd372116d7e9846466ea1212188281
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-21 19:40:41 +0200

    domaci i push metode

--- Javascript 2. ciklus/20. Cas/app.js
+++ Javascript 2. ciklus/20. Cas/app.js
@@ -87,14 +87,3 @@ console.log(maximum);
 // 	return minimum;
 // };
 // console.log(myfunction([12, -32, 32, 13, 52, 32, 17]));
-
-const myfunction = (arr) => {
-	let maximum = Number.NEGATIVE_INFINITY;
-	for (number of arr) {
-		if (number > maximum) {
-			maximum = number;
-		}
-	}
-	return maximum;
-};
-console.log(myfunction([12, -32, 32, 13, 52, 32, 17]));
