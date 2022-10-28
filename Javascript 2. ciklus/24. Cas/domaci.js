2ed9538edabc590e70aef48ca4d4a3d1a15fd8fa
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-28 11:48:05 +0200

    Domaci zadatak

--- Javascript 2. ciklus/24. Cas/domaci.js
+++ Javascript 2. ciklus/24. Cas/domaci.js
@@ -32,5 +32,15 @@ console.log(myfunction());
 // Treci deo. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
 // Funkcija na kraju treba da vrati dva napravljena pomocna niza.
 
-// const DATA1 = [2, 26, 38, 75, 11, 29];
-// const DATA2 = ["a", "b", "c", "d", "e"];
+const DATA1 = [2, 26, 38, 75, 11, 29];
+const DATA2 = ["a", "b", "c", "d", "e"];
+
+const myfunction2 = () => {
+	const data1idata2 = DATA1.concat(DATA2);
+	const parnibrojevi = DATA1.filter((element) => {
+		return element % 2 === 0;
+	});
+	parnibrojevi.push(10, 20);
+	return data1idata2, parnibrojevi;
+};
+console.log(myfunction2());
