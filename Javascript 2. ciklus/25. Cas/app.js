f4f87d5eb0703425a2c4435a8ca087709f1c2eb1
abdurahman <selmanovicabdurrahman@gmail.com>
2022-10-31 20:28:58 +0100

    Zadatci sa filter map reduce metodama

--- Javascript 2. ciklus/25. Cas/app.js
+++ Javascript 2. ciklus/25. Cas/app.js
@@ -1,10 +1,36 @@
-const nizBrojeva = [2, 26, 38, 75, 11, 29];
-function firstTask(arr) {
-	const duzina = arr.length;
-	const suma = arr.reduce((prev, curr) => {
-		return prev + curr;
-	});
-	return +(suma / duzina).toFixed(2);
-}
+// 1.
+// From array of numbers, choose even double numbers, and compute
+// the sum using array filter, map and reduce methods.
 
-console.log(firstTask(nizBrojeva));
+// 2.
+// iz niza od 10 elemenata izracunati sumu parnih brojeva niza.
+
+const brojevi = [12, 20, 13, 27, 18, 32, 2];
+// 1. zadatak
+const myfunction = (arr) => {
+	const noviniz = arr
+		.filter((element) => {
+			return element % 2 === 0;
+		})
+		.map((element) => {
+			return element * 2;
+		})
+		.reduce((prev, curr) => {
+			return prev + curr;
+		});
+	return noviniz;
+};
+
+console.log(myfunction(brojevi));
+// 2. zadatak
+const myfunction2 = (arr2) => {
+	const niz = arr2
+		.filter((element) => {
+			return element % 2 === 0;
+		})
+		.reduce((prev, curr) => {
+			return prev + curr;
+		});
+	return niz;
+};
+console.log(myfunction2(brojevi));
