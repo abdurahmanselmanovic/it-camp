ba1c5182d4f2318a7595e3cee791c7a8c5f0a63c
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-02 18:19:41 +0100

    DOMACI

--- Javascript 2. ciklus/26. Cas/domaci.js
+++ Javascript 2. ciklus/26. Cas/domaci.js
@@ -1,8 +1,8 @@
 // Broj palindrom se cita isto sa obe strane. Najveci palindrom napravljen od proizvoda dva
 //  dvocifrena broja je 9009 = 91 x 99.
 //  Pronaci najveci palindrom napravljen od proizvoda dva trocifrena broja.
+// 913 * 993 906609
 //najveci palindrom
-// 913 * 993
 najveciPalindrom = 0;
 for (let i = 999; i >= 100; i--) {
 	for (let x = 999; x >= 100; x--) {
