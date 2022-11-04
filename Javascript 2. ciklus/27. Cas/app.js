5cca247c1db14c4f8967a76f25300d3d7faf1d9e
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-04 20:38:12 +0100

    lastIndexOf(),indexOf(),keys(),entires()

--- Javascript 2. ciklus/27. Cas/app.js
+++ Javascript 2. ciklus/27. Cas/app.js
@@ -24,3 +24,36 @@ console.log(myfunction(niz));
 
 // lastIndexOf() -  metoda pretrazuje niz i vraca nam index poslednjeg pojavljivanja elementa koji predstavlja argument date metode.()
 // Ako se element koji pretrazujemo ne nalazi unutar niza,
+
+// includes() metoda funkcionise isto kao kod stringova. Zapravo vraca na boolean u zavisnosti od toga da li se argument nalazi na unutar niza na koji primenjujemo datu metodu.
+
+console.log(niz.includes("Mejra"));
+console.log(niz.includes("Talib"));
+
+// Javascript keys() i entries() metode nam vracaju indekse datog niza, odnosno kombinaciju indeksa i vrednosti
+
+const indeksi = niz.keys(); // Array iteration Object
+
+for (let indeks of indeksi) {
+	console.log(indeks);
+}
+
+const entriesi = niz.entries();
+
+for (let entitet of entriesi) {
+	console.log(entitet);
+}
+
+// flat() metoda iz niza nizova pravi jedan niz koji je sacinjen
+
+const svastara = [
+	["jabuke", "kruske", "mandarine", "mango"],
+	["somun", "ruze", "bageto", "francuski"],
+	["krompir", "bostan", "luk"],
+];
+
+const svastaraSredjena = svastara.flat();
+console.log(svastaraSredjena);
+
+const odAdoZ = svastaraSredjena.sort();
+console.log(odAdoZ);
