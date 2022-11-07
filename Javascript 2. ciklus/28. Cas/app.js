369b7f4f212b4113a389b54c9e82a5324fdc69d8
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-07 20:43:15 +0100

    Hoisting, truthy falsy

---
+++ Javascript 2. ciklus/28. Cas/app.js
@@ -0,0 +1,7 @@
+// Hoisting kod funkcija vazi ako se radi o funkcijama definisanim preko function keyword, dok kod arrow funkcija ne vazi (nije moguce pozvati funkciju pre definisanja iste).
+
+// Da li je neka vrednost TRUTHY ili FALSY
+console.log(Boolean(!!"Muhamed"));
+
+const niz = [1, 2, 3, 4, 5, 6, 7];
+console.log(sizeOf(niz));
