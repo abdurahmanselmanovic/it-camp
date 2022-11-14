5aa49a706239b3bdc6bb610428c3127aae0f741d
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-14 20:47:41 +0100

    Zadatak

---
+++ Javascript 2. ciklus/31. Cas/app.js
@@ -0,0 +1,27 @@
+// Napraviti objekat sa sledecim svojstvima:
+// 1. firstName,
+// 2. lastName,
+// 3. language,
+// 4. setLanguage - metoda za setovanje jezeika
+// 5. setNickName - metoda sastavljen od prva dva slova imena i prva dva slova prezimena
+
+const myObject = {};
+
+myObject.firstName = "Abdurahman";
+myObject.lastName = "Selmanovic";
+myObject.language = "English";
+myObject.setLaunguage = function (jezik) {
+	if (this.language !== jezik) {
+		this.language = jezik;
+	}
+};
+myObject.nickName = " ";
+myObject.setNickName = function () {
+	this.nickName =
+		this.firstName[0] + this.firstName[1] + this.lastName[0] + this.lastName[1];
+};
+console.log(myObject.setLaunguage("srpski"));
+console.log(myObject.setNickName());
+console.log(myObject);
+console.log(myObject.language);
+console.log(myObject.nickName);
