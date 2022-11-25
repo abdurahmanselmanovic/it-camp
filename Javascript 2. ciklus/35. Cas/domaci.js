b393769f899f92d3f484174a25469480fb139e7a
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-25 20:09:41 +0100

    Domaci

--- Javascript 2. ciklus/35. Cas/domaci.js
+++ Javascript 2. ciklus/35. Cas/domaci.js
@@ -4,3 +4,20 @@
 //  containing the squares of only the positive integers
 //  (decimal numbers are not integers) when an array of real numbers is passed to it.
 //   An example of an array of real numbers is [-3, 4.8, 5, 3, -3.2].
+
+const niz = [-3, 4.8, 5, 3, -3.2];
+
+function squareList(arr) {
+	const filtered = arr
+		.filter(function (number) {
+			return number > 0 && Number.isInteger(number); // && number % 1 === 0;
+		})
+		.map(function (element) {
+			return element * element;
+		});
+	return filtered;
+}
+
+console.log(squareList(niz));
+
+// Number.isInteger metoda vraca boolean u zavisnosti od toga dali je argument date metode ceo broj ili nije.
