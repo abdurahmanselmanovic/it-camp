cefd47101721bd64029fbb06323cd8f2762ac086
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-25 21:03:43 +0100

    Zadatci

--- Javascript 2. ciklus/35. Cas/app.js
+++ Javascript 2. ciklus/35. Cas/app.js
@@ -13,3 +13,78 @@ function eraseDuplicate(arr) {
 }
 
 console.log(eraseDuplicate(niz));
+
+// const eraseDuplicate2 = (arr) => {
+// 	let newarr = arr.filter((element) => {
+// 		return !newarr.includes(element);
+// 	});
+// 	return newarr;
+// };
+
+// console.log(eraseDuplicate2(niz));
+
+// ... spread operator nam sluzi za pravljenje kopije niza ili objekta, kao i spajanje vise nizova(objekata).
+
+const niz1 = ["sto", "stolica", "tabla", "marker"];
+const niz2 = [...niz1, "kuca"];
+console.log(niz2);
+// spajanje nizova
+const niz3 = [...niz, ...niz1];
+console.log(niz3);
+const niz4 = [15, ...niz2, 150];
+console.log(niz4);
+
+const obj = {
+	ime: "Muhamed",
+	prezime: "Krkmisevic",
+	starost: 17,
+};
+
+const newobj = {
+	...obj,
+	bojakose: "crna",
+	kovrdzav: false,
+};
+console.log(newobj);
+
+// Napraviti funkciju koja vraca novi niz, gde je svaki element objekat sa vrednostima title i budget.
+
+const movies = [
+	{
+		title: "Iception",
+		rating: 8,
+		budget: "230M",
+	},
+	{
+		title: "Interstelar",
+		rating: 5,
+		budget: "200M",
+	},
+	{
+		title: "GodFather",
+		rating: 9,
+		budget: "280M",
+	},
+	{
+		title: "Home Alone",
+		rating: 4,
+		budget: "300M",
+	},
+	{
+		title: "Bad Boys",
+		rating: 8,
+		budget: "180M",
+	},
+];
+
+function just2(arr) {
+	const newarr = arr.map(function (element) {
+		return {
+			title: element.title,
+			budget: element.budget,
+		};
+	});
+	return newarr;
+}
+
+console.log(just2(movies));
