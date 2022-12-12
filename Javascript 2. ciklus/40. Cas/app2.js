3cf56d5346f8f81f10aa47d7628279c8cc1242a5
abdurahman <selmanovicabdurrahman@gmail.com>
2022-12-12 20:13:21 +0100

    import export

---
+++ Javascript 2. ciklus/40. Cas/app2.js
@@ -0,0 +1,7 @@
+// JavaScript moduli (modules) //
+
+// Moduli nam dozvoljavaju da podelimo nas kod u vise fajlova.
+// Za pravljenje modula se koriste
+// import i export statements
+
+// statements (naredbe).
