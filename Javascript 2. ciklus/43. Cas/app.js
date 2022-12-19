0b72aee752eb66a022fcea9ead8fc8f0c5d329dd
abdurahman <selmanovicabdurrahman@gmail.com>
2022-12-19 20:44:22 +0100

    callback asynchronus js

---
+++ Javascript 2. ciklus/43. Cas/app.js
@@ -0,0 +1,53 @@
+// console.log(1);
+// console.log(2);
+
+// setTimeout(() => {
+// 	console.log(3);
+// }, 1000);
+
+// console.log(4);
+
+// setTimeout(() => {
+// 	console.log("ss");
+// }, 950);
+
+// arr = undefined;
+
+// console.log("hello world");
+
+// setTimeout(() => {
+// 	arr = [2, 12, 32];
+// }, 1000);
+
+// arr.forEach((element) => {});
+
+// function glavniPosao(arg, time) {
+// 	console.log("glavni posao");
+// 	arg();
+// }
+
+// function pomocniPosao() {
+// 	console.log("pomocni posao");
+// 	console.log("pomocni posao");
+// 	console.log("pomocni posao");
+// 	console.log("pomocni posao");
+// 	console.log("pomocni posao");
+// }
+
+// function callback() {
+// 	console.log("callback");
+// 	console.log("callback");
+// }
+// glavniPosao(callback);
+// setTimeout(callback, 1000);
+
+function func1() {
+	console.log("lalalalla");
+}
+
+function callback() {
+	console.log("\n ss");
+}
+
+setTimeout(callback, 5000);
+func1();
