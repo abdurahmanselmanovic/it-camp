e1f6c34068911c9f426cea3e98d06fc3bfd32e4b
abdurahman <selmanovicabdurrahman@gmail.com>
2022-12-23 20:23:50 +0100

    Asynchronous promise

--- Javascript 2. ciklus/44. Cas/app.js
+++ Javascript 2. ciklus/44. Cas/app.js
@@ -5,3 +5,18 @@ console.log(4);
 setTimeout(() => console.log(5), 0);
 
 // Promise je JS objekat koji predstavlja mesto za rezultate asinhrone funkcije sve dok traje izvrsavanje asinhone operacije.
+
+const imatiCas = new Promise((resolve, reject) => {
+	const response = 100;
+	if (response === 200) {
+		resolve();
+	} else {
+		reject();
+	}
+});
+
+imatiCas
+	.then(() => console.log("Odrzana je rec. Imali smo cas!"))
+	.catch(() => {
+		console.log("Nismo mali cas");
+	});
