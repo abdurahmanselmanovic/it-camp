650ce72aac8a94cab35c1676b414353dca3e972a
abdurahman <selmanovicabdurrahman@gmail.com>
2022-12-28 19:22:39 +0100

    fetch

---
+++ Javascript 2. ciklus/45. Cas/app5.js
@@ -0,0 +1,6 @@
+// fetch() asihrona funkcija za prikupljanje nekih podataka.
+
+fetch("https://jsonplaceholder.typicode.com/posts/")
+	.then((response) => response.json())
+	.then((posts) => console.log(posts))
+	.catch((err) => console.log(err));
