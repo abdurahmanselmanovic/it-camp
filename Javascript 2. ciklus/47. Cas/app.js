c9dcb4f5eababb3f8484e42de9fa3f14aa573666
abdurahman <selmanovicabdurrahman@gmail.com>
2022-12-30 21:03:18 +0100

    class in js

--- Javascript 2. ciklus/47. Cas/app.js
+++ Javascript 2. ciklus/47. Cas/app.js
@@ -5,37 +5,37 @@
 
 // fs.writeFile("text.txt", "sadasd");
 
-class car {
-	constructor(name, type) {
-		this.name = name;
-		this.type = type;
-	}
-	fullName() {
-		return console.log(this.name + " " + this.type);
-	}
-}
+// class car {
+// 	constructor(name, type) {
+// 		this.name = name;
+// 		this.type = type;
+// 	}
+// 	fullName() {
+// 		return console.log(this.name + " " + this.type);
+// 	}
+// }
 
-const newCar = new car("audi", "limuzina");
+// const newCar = new car("audi", "limuzina");
 
-console.log(newCar);
+// console.log(newCar);
 
-newCar.fullName();
+// newCar.fullName();
 
-class User {
-	constructor(name, surname) {
-		this.name = name;
-		this.surname = surname;
-	}
-	fullName() {
-		return console.log(this.name + " " + this.surname);
-	}
-}
+// class User {
+// 	constructor(name, surname) {
+// 		this.name = name;
+// 		this.surname = surname;
+// 	}
+// 	fullName() {
+// 		return console.log(this.name + " " + this.surname);
+// 	}
+// }
 
-const newUser = new User("abdurahman", "selmanovic");
+// const newUser = new User("abdurahman", "selmanovic");
 
-console.log(newUser);
+// console.log(newUser);
 
-newUser.fullName();
+// newUser.fullName();
 
 // Create a new ckass user with data like name, lName, yearsOfBirth
 
@@ -55,3 +55,53 @@ const newUser1 = new User1("abdurahman", "selmanovic", 2008);
 
 console.log(newUser1);
 newUser1.godine();
+
+class AdditionalInfo extends User1 {
+	statesArr = [
+		{
+			state: "USA",
+			cId: "+1",
+		},
+		{
+			state: "UK",
+			cId: "+44",
+		},
+		{
+			state: "Mexico",
+			cId: "+52",
+		},
+		{
+			state: "India",
+			cId: "+91",
+		},
+		{
+			state: "China",
+			cId: "+86",
+		},
+		{
+			state: "Serbia",
+			cId: "+381",
+		},
+	];
+	constructor(street, city, state, phoneNumber) {
+		super(newUser1.name, newUser1.lname, newUser1.age, newUser1.yearOfBirth);
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.phoneNumber = phoneNumber;
+		this.number = this.numberHandler(this.state, this.phoneNumber);
+	}
+	numberHandler(state, phoneNumber) {
+		this.findState = this.statesArr.find((s) => s.state === state);
+		return phoneNumber.replace(phoneNumber[0], this.findState.cId);
+	}
+}
+
+const Additional = new AdditionalInfo(
+	"rifata b",
+	"novi pazar",
+	"Serbia",
+	"074213123"
+);
+
+console.log(Additional);
