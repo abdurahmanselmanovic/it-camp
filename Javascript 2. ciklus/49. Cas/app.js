7bdaa70029b35a52644cd344eb68f968944159bb
abdurahman <selmanovicabdurrahman@gmail.com>
2023-01-06 20:53:34 +0100

    dom

--- Javascript 2. ciklus/49. Cas/app.js
+++ Javascript 2. ciklus/49. Cas/app.js
@@ -7,3 +7,41 @@
 // D Document
 // O Object
 // M Manipulation
+// I sluzi za maniplaciju elementima html-a kroz JS kod
+
+// var nekiElement = document.getElementById("nekiElement");
+
+// nekiElement.style.color = " #02551";
+
+var noviElement = document.getElementById("ime"); // funkcija u jsu koja omogucava pristup elementu putem njegovog id-a
+
+ime.innerText = "abdurahman";
+noviElement.style.color = "	#89CFF0"; // style je nacin pristupa i izmene css-a elementa
+noviElement.style.fontSize = "40px";
+
+var prazan1 = document.getElementById("prezime");
+
+prazan1.innerText = "selmanovic"; // innerText je nacin izmene ili popunjavanja texta unutar elementa, innerHtml je za unos html elemenata
+
+prazan1.style.color = "	#000    ";
+prazan1.style.fontSize = "40px"; // promena velicina fonta kroz css
+
+var natureImg = document.getElementById("nature");
+
+natureImg.style.borderRadius = "50%";
+natureImg.style.width = "300px";
+natureImg.style.height = "300px";
+
+natureImg.addEventListener("click", () => {
+	natureImg.style.display = "none";
+	setTimeout(() => {
+		natureImg.style.display = "block";
+	}, 1000);
+});
+// addEventListener je funkcija koja se dodaje html elementu koji smo naznacili
+// pradi dogadjanja oko ili unutar naznacenog elementa i izvrsava funkciju koja mu je zadata
+// prvi argument funkcije je event (sta treba da korisnik da uradi) drugi argument je funkcija koju treba
+// program da obavi
+console.log(natureImg.getAttribute("src"));
+
+// getAttribute je funkcija koja uzima unete podatke atributa koji se nalaze unutar elemenata koji smo naznacili
