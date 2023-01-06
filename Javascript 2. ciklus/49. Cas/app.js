e6077a1ed85ee5013557e3c94d6b5b2b89685073
abdurahman <selmanovicabdurrahman@gmail.com>
2023-01-06 20:04:20 +0100

    dom

---
+++ Javascript 2. ciklus/49. Cas/app.js
@@ -0,0 +1,9 @@
+//  cd change directory je komanda u command promptu koja sluzi za navigiranje kroz foldere unutar sistema
+
+// ls - je komanda koja sluzi za izlistavanje fajlova i foldera unutar foldera
+
+// mkdir - make directory je komanda koja sluzi za kreiranje novog direktorija ili foldera
+
+// D Document
+// O Object
+// M Manipulation
