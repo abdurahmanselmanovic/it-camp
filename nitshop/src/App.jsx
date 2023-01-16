30ff2b962238963c62fc51ed01213a1997bee30f
abdurahman <selmanovicabdurrahman@gmail.com>
2023-01-16 20:44:03 +0100

    styled button

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { StyledButton } from "./components/StyledButton/StyleButton";
+import StyledHeader from "./components/StyledHeader/styleHeader";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.....
-				</p>
+				<StyledHeader></StyledHeader>
+				<StyledButton innerText="CLICK"></StyledButton>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
---
+++ nitshop/src/components/StyledHeader/styleHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styleHeader.css";
+
+const StyledHeader = () => {
+	return <h1 className="styledHeader">NEKI H1</h1>;
+};
+
+export default StyledHeader;
