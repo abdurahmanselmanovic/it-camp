aff773c549a662da8420daf9c8546a162b3bc835
abdurahman <selmanovicabdurrahman@gmail.com>
2023-02-06 20:03:20 +0100

    styled-components

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
 import GridCmp from "./components/Grid/GridCmp";
+import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
 
 const App = () => {
-	return (
-		<>
-			<GridCmp />
-		</>
-	);
+	return <PrimaryButton>Primary Button</PrimaryButton>;
 };
 
 export default App;
---
+++ nitshop/src/components/PrimaryButton/PrimaryButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const PrimaryButton = styled.button`
+	border: 2px solid pink;
+	color: pink;
+	width: 200px;
+	height: 50px;
+	background-color: white;
+	padding: 20px;
+	font-family: Arial, Helvetica, sans-serif;
+`;
+
+export default PrimaryButton;
