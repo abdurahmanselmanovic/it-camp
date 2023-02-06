30af92c6a0a18132044702fd24536b5c4fefb8af
abdurahman <selmanovicabdurrahman@gmail.com>
2023-02-06 20:48:03 +0100

    styled-components

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -5,9 +5,18 @@ import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
 
 const App = () => {
 	return (
-		<div>
-			<PrimaryButton primary>Primary Button</PrimaryButton>{" "}
-			<PrimaryButton>ss</PrimaryButton>
+		<div
+			style={{
+				display: "flex",
+				height: 400,
+				backgroundColor: "red",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<PrimaryButton primary width>
+				Primary Button
+			</PrimaryButton>{" "}
 		</div>
 	);
 };
--- nitshop/src/components/PrimaryButton/PrimaryButton.jsx
+++ nitshop/src/components/PrimaryButton/PrimaryButton.jsx
@@ -4,10 +4,9 @@ import styled from "styled-components";
 const PrimaryButton = styled.button`
 	border: 2px solid pink;
 	color: ${(props) => (props.primary ? "blue" : "black")};
-	width: 200px;
-	height: 50px;
+	width: ${(props) => (props.primary ? "150px" : "250px")};
+	height: ${(props) => (props.primary ? "50px" : "100px")};
 	background-color: ${(props) => (props.primary ? "white" : "pink")};
-	padding: 20px;
 `;
 
 export default PrimaryButton;
