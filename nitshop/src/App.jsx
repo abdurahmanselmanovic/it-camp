eebff4b254e77d01856d85fbca642fbb3ccfc711
abdurahman <selmanovicabdurrahman@gmail.com>
2023-02-08 19:52:14 +0100

    navbar

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -5,11 +5,7 @@ import PrimaryDiv from "./components/PrimaryButton/PrimaryButton";
 import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
 
 const App = () => {
-	return (
-		<PrimaryDiv primary>
-			<h1>sss</h1>
-		</PrimaryDiv>
-	);
+	return ss;
 };
 
 export default App;
--- nitshop/src/components/PrimaryButton/PrimaryButton.jsx
+++ nitshop/src/components/PrimaryButton/PrimaryButton.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-// const PrimaryButton = styled.button`
-// 	border: 2px solid pink;
-// 	color: ${(props) => (props.primary ? "blue" : "black")};
-// 	width: ${(props) => (props.primary ? "150px" : "250px")};
-// 	height: ${(props) => (props.primary ? "50px" : "100px")};
-// 	background-color: ${(props) => (props.primary ? "white" : "pink")};
-// `;
-
-// export default PrimaryButton;
-
-const PrimaryDiv = styled.div`
-	color: ${(props) => (props.primary ? "white" : "black")};
-	width: ${(props) => (props.primary ? "80%" : "100%  ")};
-	background-color: ${(props) => (props.primary ? "blue" : "red")};
+const PrimaryButton = styled.button`
+	border: 2px solid pink;
+	color: ${(props) => (props.primary ? "blue" : "black")};
+	width: ${(props) => (props.primary ? "150px" : "250px")};
+	height: ${(props) => (props.primary ? "50px" : "100px")};
+	background-color: ${(props) => (props.primary ? "white" : "pink")};
 `;
 
-export default PrimaryDiv;
+export default PrimaryButton;
