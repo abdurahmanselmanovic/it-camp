097ca80996e4072a2c19a1fb34feb6c35df853c6
abdurahman <selmanovicabdurrahman@gmail.com>
2023-02-13 19:37:39 +0100

    header

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-import GridCmp from "./components/Grid/GridCmp";
-import PrimaryDiv from "./components/PrimaryButton/PrimaryButton";
-import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
 import Navigation from "./Templates/Navigation/Navigation";
 
 const App = () => {
--- nitshop/src/Templates/Navigation/Navigation.jsx
+++ nitshop/src/Templates/Navigation/Navigation.jsx
@@ -1,14 +1,11 @@
-import { Grid } from "@mui/material/";
 import React from "react";
-import CustomDiv from "../../components/CustomDiv/CustomDiv";
+import Grid from "@mui/material/Grid";
 import TopRow from "./TopRow";
 
 const Navigation = () => {
 	return (
 		<Grid container direction="row">
-			<CustomDiv padding="0px 5p%" border="1px" width="100%">
-				<TopRow></TopRow>
-			</CustomDiv>
+			<TopRow />
 		</Grid>
 	);
 };
